1cb051854eb08f9423e44767dcc835aaf87bc64d
feihongbin <1909427579@qq.com>
2021-05-20 21:24:55 +0800

    third day

--- learning/src/components/index.js
+++ learning/src/components/index.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import Clock from './clock'
 import CourseField from './courseField'
 import CourseList from './courseList'
 
 export { Clock, CourseField, CourseList }
-=======
-import Welcome from './comps/welcome'
-import NameForm from './comps/normalForm'
-
-export {Welcome,NameForm}
->>>>>>> 487d343913532f1ee4a5f8b7e7c51817fe3b9c0f
